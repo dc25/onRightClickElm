9247b98c368eb7e4e814d3ce79b50aa0a337b8a8
Dave Compton <davecompton7@gmail.com>
2016-12-13 21:41:31 +0000

    added svg input area

--- onRightClick.elm
+++ onRightClick.elm
@@ -1,16 +1,47 @@
-import Html exposing (beginnerProgram, div, button, text)
+import Html as HTML exposing (beginnerProgram, div, button, text)
 import Html.Events exposing (onClick, onWithOptions)
 import Json.Decode as Json
+import Svg exposing (svg, text_, text, rect, g)
+import Svg.Attributes exposing (x, y, height, width, version, style, fontSize, dominantBaseline, fill)
 
 main =
   beginnerProgram { model = 0, view = view, update = update }
 
+svgBox model = 
+    let w = 600
+        h = 40
+
+    in svg [ version "1.1"
+           , width (toString w)
+           , height (toString h)
+           ] 
+           [ g [] 
+               [ rect [ width (toString w)
+                      , height (toString h) 
+                      , fill "grey"
+                      , onClick Increment
+                      , onRightClick Decrement
+                      ] 
+                      []
+               , text_ [ x "10"
+                       , y (toString (h/2))
+                       , fontSize (toString (h-10))
+                       , dominantBaseline "middle"
+                       , onClick Increment
+                       , onRightClick Decrement
+                       ] 
+                       [ Svg.text "click left or right to decrement or increment" 
+                       ]
+              ]
+           ]
+
 
 view model =
   div []
-    [ button [ onRightClick Decrement ] [ text "click right to decrement" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "click left to increment" ]
+    [ button [ onRightClick Decrement ] [ HTML.text "click right to decrement" ]
+    , div [] [ HTML.text (toString model) ] , button [ onClick Increment ] [ HTML.text "click left to increment" ]
+    , div [] [ ]
+    , svgBox model
     ]
 
 onRightClick message =
